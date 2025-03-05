893f7ee90f04718e8990652878fc80eb5d3ba44d
Lance Armah-Abraham <lance@vdc.cloud>
2025-03-05 10:08:46 +0000

    feat: add shadcn components and update home

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,11 +1,24 @@
+import { Card, CardContent, CardHeader, CardTitle } from "~/components/ui/card";
 import type { Route } from "./+types/_index";
+import { Button } from "~/components/ui/button";
+import { Link } from "react-router";
 
 export const meta = ({}: Route.MetaArgs) => [{ title: "Homepage" }];
 
 export default function Page() {
   return (
-    <div>
-      <h1>Homepage</h1>
+    <div className="flex items-center justify-center h-svh">
+      <Card className="mx-auto max-w-sm w-full">
+        <CardHeader>
+          <CardTitle>Welcome</CardTitle>
+        </CardHeader>
+
+        <CardContent>
+          <Button asChild>
+            <Link to="/contacts">Go to Contacts</Link>
+          </Button>
+        </CardContent>
+      </Card>
     </div>
   );
 }
--- app/routes/contacts.create.tsx
+++ app/routes/contacts.create.tsx
@@ -2,6 +2,10 @@ import type { Route } from "./+types/contacts.create";
 
 export const meta = ({}: Route.MetaArgs) => [{ title: "Create Contact" }];
 
+export const loader = ({}) => {
+  return {};
+};
+
 export default function Page() {
   return (
     <div>
